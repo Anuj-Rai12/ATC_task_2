703d032e24461ed14c0d3ac8b2f5780924869373
Anuj <anujrai04082002.0@gmail.com>
2021-08-25 17:09:26 +0530

    Add Navigation Bar status:Successfully

--- app/src/main/java/com/example/cargo/MainActivity.kt
+++ app/src/main/java/com/example/cargo/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.cargo.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,17 +15,20 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        navController = navHostFragment.findNavController()
-        setupActionBarWithNavController(navController)
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.root)
+        binding.nagHomeGal.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/cargo/ui/GalleryDetailFragment.kt
+++ app/src/main/java/com/example/cargo/ui/GalleryDetailFragment.kt
@@ -1,20 +1,22 @@
 package com.example.cargo.ui
 
-import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.cargo.R
 import com.example.cargo.databinding.GalleryDetailFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class GalleryDetailFragment :Fragment(R.layout.gallery_detail_fragment){
+class GalleryDetailFragment : Fragment(R.layout.gallery_detail_fragment) {
     private lateinit var binding: GalleryDetailFragmentBinding
-    @SuppressLint("ResourceAsColor")
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding= GalleryDetailFragmentBinding.bind(view)
-        binding.galImageFull.setBackgroundColor(resources.getColor(R.color.black))
+        binding = GalleryDetailFragmentBinding.bind(view)
+        binding.galImageFull.setBackgroundColor(resources.getColor(R.color.black, null))
     }
 }
