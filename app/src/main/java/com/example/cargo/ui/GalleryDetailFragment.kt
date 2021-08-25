0ac1e20b688cbbe5baa525a85932d4670db60993
Anuj <anujrai04082002.0@gmail.com>
2021-08-25 15:28:13 +0530

    Added Zoom Feature status:Successfully

--- app/src/main/java/com/example/cargo/ui/GalleryDetailFragment.kt
+++ app/src/main/java/com/example/cargo/ui/GalleryDetailFragment.kt
@@ -1,9 +1,9 @@
 package com.example.cargo.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.cargo.R
 import com.example.cargo.databinding.GalleryDetailFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,12 +11,10 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class GalleryDetailFragment :Fragment(R.layout.gallery_detail_fragment){
     private lateinit var binding: GalleryDetailFragmentBinding
+    @SuppressLint("ResourceAsColor")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding= GalleryDetailFragmentBinding.bind(view)
-        binding.galImageFull.setOnClickListener {
-            val action=GalleryDetailFragmentDirections.actionGlobalMyDialog(title = "Hello","I'm Anuj")
-            findNavController().navigate(action)
-        }
+        binding.galImageFull.setBackgroundColor(resources.getColor(R.color.black))
     }
 }
