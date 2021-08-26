5bb7900a3e03fc3d2d1132d4e25a8ba59c2aafc8
Anuj <anujrai04082002.0@gmail.com>
2021-08-26 12:55:50 +0530

    Removed Nav Drawer Bugs status:Successfully

--- app/src/main/java/com/example/cargo/MainActivity.kt
+++ app/src/main/java/com/example/cargo/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
 import com.example.cargo.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -30,6 +31,11 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(navController.graph, binding.root)
+        binding.nagHomeGal.setupWithNavController(navController)
         binding.toolbarLayoutAll.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
