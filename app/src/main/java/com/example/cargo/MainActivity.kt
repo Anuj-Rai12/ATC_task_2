d103b673451cde1147578e15eba7953cbe2d6552
Anuj <anujrai04082002.0@gmail.com>
2021-08-27 09:06:27 +0530

    Removed Action Bar Bugs status:Successfully

--- app/src/main/java/com/example/cargo/MainActivity.kt
+++ app/src/main/java/com/example/cargo/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         toolbar = binding.toolbarLayoutAll
